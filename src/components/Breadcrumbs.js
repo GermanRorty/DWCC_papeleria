bb7f7865734a757390347389b7cd4ac268658c08
German R. Marty <germanr.marty@outlook.es>
2025-02-19 02:53:47 +0100

    in-progress: breadcrumbs reestructurados

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -1,28 +1,30 @@
-'use client';
+"use client";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const Breadcrumbs = () => {
-    const pathName = usePathname();
-    const pathSegments = pathName.split("/").filter((segment)=>segment); // El filter((x) => x) garantiza que no habrá elementos vacíos en el array resultante
-    return(
-            <div>Inicio
-                {pathSegments.map((segment, index)=>{
-                    const progresivePath = `/${pathSegments.slice(0, index + 1).join("/")}`;
-                    if(index === pathSegments.length - 1 ){
-                        return(
-                            <span key="index">{segment}</span> // El final de la ruta corresponde al nombre del artículo
-                        );
-                    }
-                    return(
-                        <div>
-                            &gt; <Link href={progresivePath}>{segment}</Link>
-                        </div>
-                    ); 
-                })}
-            </div>
-    );
-}
+	const pathName = usePathname();
+	const pathSegments = pathName.split("/").filter((segment) => segment); // El filter((x) => x) garantiza que no habrá elementos vacíos en el array resultante
+	return (
+		<div>
+			<Link href={"/"}>Inicio</Link>
+			{pathSegments.map((segment, index) => {
+				const progresivePath = `/${pathSegments.slice(0, index + 1).join("/")}`;
+                console.log(progresivePath);
+				if (index === pathSegments.length - 1) {
+					return (
+						<span key={index}>&gt;{segment}</span> // El final de la ruta corresponde al nombre del artículo
+					);
+				}
+				return (
+					<Link key={index} href={progresivePath}>
+						{segment}
+					</Link>
+				);
+			})}
+		</div>
+	);
+};
 
 export default Breadcrumbs;
