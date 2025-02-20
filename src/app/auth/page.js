b1960f2f2ade667ce64bc97e840558fd798a0596
German R. Marty <germanr.marty@outlook.es>
2025-02-20 02:52:49 +0100

    fix: proceso de logout con borrado de carrito localStorage

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -1,19 +1,40 @@
 "use client";
+import { syncUserCartToDB } from "@/lib/services/carrito";
 import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Home() {
-  const { data: session } = useSession();
+	const { data: session } = useSession();
 
-  return (
-    <div>
-      {session ? (
-        <>
-          <p>Bienvenido, {session.user.name}!</p>
-          <button onClick={() => signOut()}>Cerrar sesión</button>
-        </>
-      ) : (
-        <button onClick={() => signIn()}>Iniciar sesión</button>
-      )}
-    </div>
-  );
+	const logoutProcess = async () => {
+		const cart = JSON.parse(localStorage.getItem("cart")) || [];
+		await syncUserCartToDB(session.user.id, cart);
+    localStorage.removeItem("cart");
+		signOut();
+	};
+
+	return (
+		<div>
+			{session ? (
+				<>
+					<p>Bienvenido, {session.user.name}!</p>
+					<button onClick={logoutProcess}>Cerrar sesión</button>
+				</>
+			) : (
+				<div>
+					<button onClick={() => signIn()}>Iniciar sesión</button>
+					<Link href="/usuarios/gestion">Registrarse</Link>
+				</div>
+			)}
+		</div>
+	);
 }
+
+// console.log("Datos de sesion:", session);
+// useEffect(() => {
+// 	if (session != undefined) {
+// 		const userId = session.user.id;
+// 		console.log(userId);
+// 	}
+// }, [session]);
