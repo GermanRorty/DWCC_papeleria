ae47451a6d5d211d5f2bf7796d577242c88eca8a
German R. Marty <germanr.marty@outlook.es>
2025-02-20 02:51:49 +0100

    feat: subida de carrito a JSON server

---
+++ src/app/api/usuarios/[id]/route.js
@@ -0,0 +1,24 @@
+// app/api/usuarios/[id]/route.js
+
+const API_URL = "http://localhost:4000/usuarios";
+
+export async function PATCH(request,context) {
+	const params = await context.params; // En las Api Routes params tiene que estar disponible (hay que esperar)
+    const {id} = params;
+	try {
+        const cart = request.json();
+		const response = await fetch(`${API_URL}/${id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({cart}),
+        });
+
+		if (!response.ok) throw new Error("Error al editar el usuario");
+		const editedUser = await response.json();
+		return new Response(JSON.stringify(editedUser), { status: 201 });
+	} catch (error) {
+		console.error("Error en PATCH /usuario", error);
+		return new Response("Error al obtener el usuario", { status: 500 });
+	}
+}
+
