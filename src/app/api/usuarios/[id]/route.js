7e17abaeeb1161e1779f68ca6d90a338b927e837
German R. Marty <germanr.marty@outlook.es>
2025-02-20 18:37:17 +0100

    miscellaneous: TODO's y db

--- src/app/api/usuarios/[id]/route.js
+++ src/app/api/usuarios/[id]/route.js
@@ -1,24 +1,18 @@
-// app/api/usuarios/[id]/route.js
+// app/api/usuarios/route.js
 
 const API_URL = "http://localhost:4000/usuarios";
 
-export async function PATCH(request,context) {
-	const params = await context.params; // En las Api Routes params tiene que estar disponible (hay que esperar)
+export async function GET(request, context) {
+    const params = await context.params;
     const {id} = params;
-	try {
-        const cart = request.json();
-		const response = await fetch(`${API_URL}/${id}`, {
-            method: "PATCH",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({cart}),
-        });
+    try {
+        const response = await fetch(`${API_URL}/${id}`);
+        if(!response.ok) throw new Error(`Error al obtener el producto ID = ${id}`);
+        const producto = await response.json();
+        return new Response(JSON.stringify(producto), { status: 200 });
 
-		if (!response.ok) throw new Error("Error al editar el usuario");
-		const editedUser = await response.json();
-		return new Response(JSON.stringify(editedUser), { status: 201 });
-	} catch (error) {
-		console.error("Error en PATCH /usuario", error);
-		return new Response("Error al obtener el usuario", { status: 500 });
-	}
-}
-
+    } catch (error) {
+        console.error(`Error en GET /producto/${id}`, error);
+        return new Response(`Error al obtener el producto ID = ${id}`, { status: 500 });
+    }
+};
