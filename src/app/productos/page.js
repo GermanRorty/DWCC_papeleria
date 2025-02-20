c4903072f1c988b43b4f00ea88d345d32092da5a
German R. Marty <germanr.marty@outlook.es>
2025-02-20 02:53:19 +0100

    fix: implementacion de CartAddButton a Componente Articulo

--- src/app/productos/page.js
+++ src/app/productos/page.js
@@ -7,12 +7,14 @@
 import Image from "next/image";
 import { createContext, useContext, useEffect, useState } from "react";
 import { getProductos } from "@/lib/services/productos";
+import { useCartContext } from "../context/CartContext";
+import CartAddButton from "@/components/CartAddButton";
 
 const ProductsListContext = createContext();
 
 // Display
 const Articulo = ({ articleProp }) => {
-	const { imageUrl, name, description, price } = articleProp;
+	const { imageUrl, name, description, price, id} = articleProp;
 	return (
 		<div className="">
 			<div>
@@ -22,6 +24,8 @@ const Articulo = ({ articleProp }) => {
 			<div id="articulo-name">{name}€</div>
 			<div id="articulo-price">{price}€</div>
 			<div id="articulo-description">{description}</div>
+			{console.log("Id para el boton",id)}
+			<CartAddButton product={articleProp}/>
 		</div>
 	);
 };
@@ -30,6 +34,7 @@ const ProductGrid = () => {
 	const {productsList, setProductsList} = useContext(ProductsListContext);
 
 	useEffect(() => {
+		// TODO: si de un articulo no quedan existencias que salga en gris 
 		console.log("Actualización de lista de productos.");
 	}, [productsList]);
 
@@ -43,36 +48,37 @@ const ProductGrid = () => {
 	);
 };
 
-const Filters = () => {
-	useEffect(() =>{
-		const fetchCategories = async() =>{
-			const products = await getCategories();
-			setProductsList(products);
-		};
-		fetchProducts();
-	},[])
+// const Filters = () => {
+// 	useEffect(() =>{
+// 		const fetchCategories = async() =>{
+// 			const products = await getCategories();
+// 			setProductsList(products);
+// 		};
+// 		fetchProducts();
+// 	},[])
 
-	return(
-		<div>
-			<h2>Filtros</h2>
-			<div>
-				<div>Categoría</div>
-				<div>
-					{categorias.map((cat)=>{
-						return(
-							<div></div>
-						);
-					})}
-				</div>
-			</div>
-		</div>
-	);
-}
+// 	return(
+// 		<div>
+// 			<h2>Filtros</h2>
+// 			<div>
+// 				<div>Categoría</div>
+// 				<div>
+// 					{categorias.map((cat)=>{
+// 						return(
+// 							<div></div>
+// 						);
+// 					})}
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// }
 
 const ProductosLayout = () => { 
 	const[productsList, setProductsList] = useState([]);
 
 	useEffect(() =>{
+		console.log("ME MONTO UNA VEZ")
 		const fetchProducts = async() =>{
 			const products = await getProductos();
 			setProductsList(products);
