76d1d706107cf9bb4d2e1324d08c0497ed16a117
German R. Marty <germanr.marty@outlook.es>
2025-02-23 15:05:49 +0100

    fix: Incorporacion de contextos a layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,10 @@ import "bootstrap/dist/css/bootstrap.min.css"; // Importa el CSS de Bootstrap gl
 import Breadcrumbs from "@/components/Breadcrumbs";
 import AuthProvider from "@/components/AuthProvider";
 import { CartContextProvider, useCartContext } from "./context/CartContext";
-import NavBarAncestor from "@/components/navbar/NavBarAncestor";
+import Header from "@/components/header/Header";
+import { ScrollYContextProvider, useScrollYContext } from "./context/ScrollYContext";
+import ScrollerWrapper from "@/components/ScrollerWrapper";
+import { ProductsListContextProvider } from "./context/ProductsContext";
 
 // TODO: Ideas para el icono de perfil: https://www.svgrepo.com/svg/109446/pencil-box https://www.svgrepo.com/svg/260522/pencil-case-school-material  https://www.svgrepo.com/svg/269236/pencil-case https://www.svgrepo.com/svg/455419/pencil-ruler https://www.svgrepo.com/svg/456626/pencil-paper https://www.svgrepo.com/svg/455416/pencil-case
 
@@ -17,19 +20,21 @@ export const metadata = {
 const RootLayout = ({ children }) => {
 	return (
 		<html lang="en">
-			<body>
-				<AuthProvider>
-					<CartContextProvider>
-						<NavBarAncestor />
-						<div className="mt-28">
-							<Breadcrumbs />
-
-							{children}
-							{/* TODO: poner aqui el componente  */}
-						</div>
-					</CartContextProvider>
-				</AuthProvider>
-			</body>
+			<ScrollYContextProvider>
+				<body className="h-screen">
+					<AuthProvider>
+						<CartContextProvider>
+							<ProductsListContextProvider>
+								<Header />
+								<ScrollerWrapper>
+									<Breadcrumbs />
+									{children}
+								</ScrollerWrapper>
+							</ProductsListContextProvider>
+						</CartContextProvider>
+					</AuthProvider>
+				</body>
+			</ScrollYContextProvider>
 		</html>
 	);
 };
