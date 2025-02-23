78460a3f7875f61d1548e40f88fc0998c603096e
German R. Marty <germanr.marty@outlook.es>
2025-02-23 12:35:26 +0100

    feat: cambiada carga de Productos a Contexto Global

---
+++ src/app/context/ProductsContext.js
@@ -0,0 +1,28 @@
+"use client";
+
+import { getProductos } from "@/lib/services/productos";
+import { useCartContext } from "./CartContext";
+
+const { createContext, useState, useContext, useEffect } = require("react");
+
+const ProductsListContext = createContext();
+
+export const ProductsListContextProvider = ({ children }) => {
+	const [productsList, setProductsList] = useState([]);
+	const { compraHecha, setCompraHecha } = useCartContext();
+
+	useEffect(() => {
+		// // DEBUG:
+		// console.log("ME MONTO UNA VEZ");
+		const fetchProducts = async () => {
+			const products = await getProductos();
+			setProductsList(products);
+		};
+		fetchProducts();
+		setCompraHecha(false);
+	}, [compraHecha]);
+
+	return <ProductsListContext.Provider value={{ productsList, setProductsList }}>{children}</ProductsListContext.Provider>;
+};
+
+export const useProductsListContext = () => useContext(ProductsListContext);
