2c3eb4d5c357c399e4ad5bbfa18f9bea8fe0ae4e
German R. Marty <germanr.marty@outlook.es>
2025-02-23 12:35:00 +0100

    fix: BreadCrumbs

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -7,23 +7,30 @@ const Breadcrumbs = () => {
 	const pathName = usePathname();
 	const pathSegments = pathName.split("/").filter((segment) => segment); // El filter((x) => x) garantiza que no habrá elementos vacíos en el array resultante
 	return (
-		<div>
-			<Link href={"/"}>Inicio</Link>
-			{pathSegments.map((segment, index) => {
-				const progresivePath = `/${pathSegments.slice(0, index + 1).join("/")}`;
-                console.log(progresivePath);
-				if (index === pathSegments.length - 1) {
+		<nav aria-label="breadcrumb">
+			<ol className="breadcrumb">
+				<li className="breadcrumb-item">
+					<Link href={"/"}>Inicio</Link>
+				</li>
+				{pathSegments.map((segment, index) => {
+					const progresivePath = `/${pathSegments.slice(0, index + 1).join("/")}`;
+					// // DEBUG:
+					// console.log(progresivePath);
+					if (index === pathSegments.length - 1) {
+						return (
+							<li className="breadcrumb-item" key={index}>
+								<span>{segment}</span> {/* // El final de la ruta corresponde al nombre del artículo */}
+							</li>
+						);
+					}
 					return (
-						<span key={index}>&gt;{segment}</span> // El final de la ruta corresponde al nombre del artículo
+						<li className="breadcrumb-item" key={index}>
+							<Link href={progresivePath}>{segment}</Link>
+						</li>
 					);
-				}
-				return (
-					<Link key={index} href={progresivePath}>
-						{segment}
-					</Link>
-				);
-			})}
-		</div>
+				})}
+			</ol>
+		</nav>
 	);
 };
 
