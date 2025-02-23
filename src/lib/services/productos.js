3550415d9a318ab90eaccd754342bfdae856d97f
German R. Marty <germanr.marty@outlook.es>
2025-02-23 15:01:23 +0100

    feat: editproductfromdatabase

--- src/lib/services/productos.js
+++ src/lib/services/productos.js
@@ -3,7 +3,6 @@
 const API_URL_PRODUCT = "http://localhost:3000/api/productos";
 const API_URL_UPLOAD_IMG = "http://localhost:3000/api/upload";
 
-
 export async function getProductos() {
 	try {
 		const response = await fetch(API_URL_PRODUCT);
@@ -42,59 +41,82 @@ export async function deleteProducto(id) {
 	}
 }
 
-
 export async function saveProductToDatabase(data) {
-	// DEBUG:
-	console.log("Data: ", data);
-	const {imgFile, ...dataNoImgFile} = data; 
+	// // DEBUG:
+	// console.log("Data: ", data);
+	const { imgFile, ...dataNoImgFile } = data;
 	try {
-        // Enviar datos del producto a la API (sin imagen)
-        const productResponse = await fetch(API_URL_PRODUCT, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(dataNoImgFile),
-        });
+		// Enviar datos del producto a la API (sin imagen)
+		const productResponse = await fetch(API_URL_PRODUCT, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(dataNoImgFile),
+		});
 
-        if (!productResponse.ok) {
-            throw new Error("Error al guardar el producto.");
-        }
+		if (!productResponse.ok) {
+			throw new Error("Error al guardar el producto.");
+		}
 
-        // Obtener el ID del producto guardado
-        const { productId } = await productResponse.json();
+		// Obtener el ID del producto guardado
+		const { productId } = await productResponse.json();
 
-        return productId; // Devolver el ID del producto
-    } catch (error) {
-        console.error("Error en saveProductToDatabase:", error);
-        throw error; // Propagar el error
-    }
+		return productId; // Devolver el ID del producto
+	} catch (error) {
+		console.error("Error en saveProductToDatabase:", error);
+		throw error; // Propagar el error
+	}
 }
 
-
-export async function uploadProductImgFs(productId, imgFile){
+export async function uploadProductImgFs(productId, imgFile) {
 	try {
-		// DEBUG:
-		console.log(imgFile);
+		// // DEBUG:
+		// console.log(imgFile);
 		const formData = new FormData();
-        formData.append("productId", productId); // Enviar el ID
-        formData.append("image", imgFile); 
+		formData.append("productId", productId); // Enviar el ID
+		formData.append("image", imgFile);
 
-        const uploadResponse = await fetch(API_URL_UPLOAD_IMG, {
-            method: "POST",
-            body: formData,
-        });
+		const uploadResponse = await fetch(API_URL_UPLOAD_IMG, {
+			method: "POST",
+			body: formData,
+		});
 
-        if (!uploadResponse.ok) {
-            throw new Error("Error al subir la imagen.");
-        }
+		if (!uploadResponse.ok) {
+			throw new Error("Error al subir la imagen.");
+		}
 
-        const { imageUrl } = await uploadResponse.json();
+		const { imageUrl } = await uploadResponse.json();
 
 		return imageUrl;
 	} catch (error) {
 		console.error("Error en uploadProductImgFs:", error);
-        throw error; // Propagar el error
+		throw error; 
 	}
-
-
 }
-  
+
+// TODO: hacer update img
+
+export async function editProductFromDatabase(data) {
+	// // DEBUG:
+	// console.log("Data: ", data);
+	const { imgFile, ...dataNoImgFile } = data;
+	try {
+		// DEBUG: 
+		console.log("ID para edit desde",dataNoImgFile.id);
+		const productResponse = await fetch(`${API_URL_PRODUCT}/${dataNoImgFile.id}`, {
+			method: "PATCH",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(dataNoImgFile),
+		});
+
+		if (!productResponse.ok) {
+			throw new Error("Error al editar el producto.");
+		}
+
+		const editedProduct = await productResponse.json();
+
+		return editedProduct.id; 
+	} catch (error) {
+		console.error("Error en editProductFromDatabase:", error);
+		throw error; 
+	}
+}
