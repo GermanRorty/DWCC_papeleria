f9d4b16e23c317431ec378cba91425c03b6abdb4
German R. Marty <germanr.marty@outlook.es>
2025-02-23 12:36:36 +0100

    fix: CartAddButton

--- src/components/CartAddButton.js
+++ src/components/CartAddButton.js
@@ -1,16 +1,18 @@
 // app/components/CartButton.js
 
 import { useCartContext } from "@/app/context/CartContext";
+import Image from "next/image";
 
-const CartAddButton = ({ product , quantity = 1}) => {
+const CartAddButton = ({ product, quantity = 1 }) => {
 	const chosenArticle = product;
-    console.log(product);
+	// // DEBUG:
+	// console.log(product);
 	const { cart, setCart } = useCartContext();
 
 	// Un boton que nos permite tanto sumar de uno en uno desde el catalogo, como incrementar/disminuir muchas unidades de golpe
 	const addToCart = (article, units) => {
 		// TODO: hacer que el stock no se guarde en el carrito, sino que se checkee vs la bbdd
-			const foundArticle = cart.find((item) => item.id === article.id);
+		const foundArticle = cart.find((item) => item.id === article.id);
 		if (foundArticle) {
 			setCart((prevCart) => prevCart.map((a) => (a.id === article.id ? { ...a, quantity: a.quantity + units } : a)));
 		} else {
@@ -18,7 +20,12 @@ const CartAddButton = ({ product , quantity = 1}) => {
 		}
 	};
 
-	return <button onClick={() => addToCart(chosenArticle, quantity)}>Agregar</button>;
+	return (
+		<button className="btn shadow" style={{ backgroundColor: "rgba(229, 216, 165, 1)" }} onClick={() => addToCart(chosenArticle, quantity)}>
+			{" "}
+			<Image alt="add to cart-backpack icon" src="images/addtobackpack.svg" width={500} height={500} className="w-5 h-6"></Image>
+		</button>
+	);
 };
 
 export default CartAddButton;
