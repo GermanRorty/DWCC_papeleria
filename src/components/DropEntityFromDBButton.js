b214fe079195a30f72e3c59c39785f4e1bd2012f
German R. Marty <germanr.marty@outlook.es>
2025-02-23 12:37:57 +0100

    feat: boton para CRUD-Delete de producto

---
+++ src/components/DropEntityFromDBButton.js
@@ -0,0 +1,24 @@
+
+import { deleteProducto } from "@/lib/services/productos";
+import { deleteUser } from "@/lib/services/users";
+import { useRouter } from "next/navigation";
+
+const DropEntityFromDBButton = ({entityId, entityClass}) => {
+    const router = useRouter();
+    const deleteFunction = () => {
+        if(entityClass === "product") deleteProducto(entityId);
+        if(entityClass === "user") deleteUser(entityId);
+    }
+
+    const redirectHome = () => {
+        router.push("/productos");
+    }
+
+    return(
+        <button className="btn btn-danger" onClick={()=>{deleteFunction(); redirectHome();}}>Eliminar</button>
+    );
+
+}
+
+
+export default DropEntityFromDBButton;
