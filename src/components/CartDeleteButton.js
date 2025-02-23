1134445f1da6e2536d47c5277408c73a64387867
German R. Marty <germanr.marty@outlook.es>
2025-02-23 12:36:56 +0100

    fix: CartDeleteButton

--- src/components/CartDeleteButton.js
+++ src/components/CartDeleteButton.js
@@ -1,17 +1,17 @@
 // app/components/CartButton.js
 
-import { useCartContext } from "@/context/CartContext";
+import { useCartContext } from "@/app/context/CartContext";
 
 const CartDeleteButton = ({ productId }) => {
 	const { setCart } = useCartContext();
 
-	const removeFromCart = (id) => {
+	const dropCart = (id) => {
         // Para evitar problemas se recomienda usar una version con prevCart en lugar de
         // setCart(cart.filter((a) => (!a.id === article.id)));
-		setCart((prevCart) => prevCart.filter((a) => a.id !== id));
+		setCart([]);
 	};
 
-	return <button onClick={() => removeFromCart(productId)}>Eliminar</button>;
+	return <button onClick={() => dropCart(productId)}>Vaciar</button>;
 };
 
 export default CartDeleteButton;
