fa0442626d089c358743fc67a4e744618f06527f
German R. Marty <germanr.marty@outlook.es>
2025-02-24 02:47:41 +0100

    fix: m,ejoras en el boton de borrado del carro

--- src/components/CartDeleteButton.js
+++ src/components/CartDeleteButton.js
@@ -6,12 +6,19 @@ const CartDeleteButton = ({ productId }) => {
 	const { setCart } = useCartContext();
 
 	const dropCart = (id) => {
-        // Para evitar problemas se recomienda usar una version con prevCart en lugar de
-        // setCart(cart.filter((a) => (!a.id === article.id)));
+		// Para evitar problemas se recomienda usar una version con prevCart en lugar de
+		// setCart(cart.filter((a) => (!a.id === article.id)));
 		setCart([]);
 	};
 
-	return <button onClick={() => dropCart(productId)}>Vaciar</button>;
+	return (
+		<div className="w-full d-flex justify-between position-sticky top-0 right-0 shadow bg-gray-200">
+			<h4 className="self-start p-3 translate-y-1">Carrito</h4>
+			<button className="btn btn-secondary w-1/6 m-3 " onClick={() => dropCart(productId)}>
+				<i className="bi bi-trash"></i>
+			</button>
+		</div>
+	);
 };
 
 export default CartDeleteButton;
