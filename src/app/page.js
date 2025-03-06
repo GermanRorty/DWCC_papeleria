5261c45e0da512277b8671669c55d61db5f1bcf8
GermanRorty <148339990+GermanRorty@users.noreply.github.com>
2025-03-06 10:09:51 +0100

    fix: Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Carousel from "@/components/Carousel";
 export default function Home() {
   return (
     <div className="position-relative w-full h-full p-0 -mt-10">
-      <h1 className="home-font position-absolute top-50 start-50 translate-middle z-50">Hola!</h1>
+      <h1 className="home-font position-absolute top-50 start-50 translate-middle z-50">Hi</h1>
       <Carousel />
     </div>
   );
